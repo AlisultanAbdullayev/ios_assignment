6f2dcbeee28a1ad4017108744575afb5b57074f5
Alisultan Abdullah <alisultanabdullaev@gmail.com>
2024-05-29 11:12:28 -0500

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Alisultan Abdullah on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
